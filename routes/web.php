5b4c44942ce686848edd57da071197750814e2f9
Lilian Levano <zey.lilianlevano@gmail.com>
2026-04-22 14:06:39 +0200

    contact controller

--- routes/web.php
+++ routes/web.php
@@ -3,3 +3,5 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [\App\Http\Controllers\WelcomeController::class, 'index']);
+
+Route::get('/contact', [\App\Http\Controllers\ContactController::class, 'index']);
